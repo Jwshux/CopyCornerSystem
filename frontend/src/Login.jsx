d1ba83ed754064e76c75a01f19fb4dbc7425bbf4
Jwshux <joshuariana05@gmail.com>
2025-10-14 18:30:17 +0800

    fix database connection

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/login", {  // Flask URL
+    const response = await fetch("http://127.0.0.1:5000/login", {  // Flask URL
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
